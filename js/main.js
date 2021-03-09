756d6bd80114ff30f5a045f529505fe476d12026
Ivannushka <ivannaorlov@gmail.com>
2021-03-10 00:49:34 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const name ='Ivanna';
+const address = 'a perfect place';
+const number = '108';
+console.log(`Hello, my name is ${name}, I live in ${address} and my favorite number is ${number}!`);
+
+
+let cars = ['Vaz', 'Zaz', 'Gaz', 'Uaz', 'Maz'];
+console.log('5th car is: ' + cars[4]);
+console.log('4th car is: ' + cars[3]);
+console.log('3rd car is: ' + cars[2]);
+console.log('2nd car is: ' + cars[1]);
+console.log('1st car is: ' + cars[0]);
